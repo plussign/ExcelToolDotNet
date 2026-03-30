1ca796dc7cf64e1bd545b8801776a77085f16ae4
plusSign <plus@vip.qq.com>
2026-03-30 15:05:13 +0800

    server update xml output codes

--- ExcelTool/ConvertTool_xml.cs
+++ ExcelTool/ConvertTool_xml.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Text;
 
 
@@ -22,7 +23,13 @@ namespace ExcelTool
                 string cellString = string.Empty;
                 if (!field.client_only)
                 {
-                    cellString = string.Format(" {0}=\"{1}\"", field.name, cellData.GetOrginalString());
+                    string cellValue = cellData.GetOrginalString();
+                    cellValue = cellValue.Replace("&", "&amp;");
+                    cellValue = cellValue.Replace("<", "&lt;");
+                    cellValue = cellValue.Replace(">", "&gt;");
+                    cellValue = cellValue.Replace("'", "&apos;");
+                    cellValue = cellValue.Replace("\"", "&quot;");
+                    cellString = string.Format(" {0}=\"{1}\"", field.name, cellValue);
                     content.Append(cellString);
                 }
             }
